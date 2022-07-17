8814a85c1e8de0eeeb46e34d61b5f5ea35d064d7
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-07-17 12:34:22 -0700

    Add more users

--- jmeter/userinfo.py
+++ jmeter/userinfo.py
@@ -2,16 +2,16 @@
 
 import csv
 
-NUM_USERS = 200
+NUM_USERS = 1000
 
 if __name__ == "__main__":
     user_list = []
     
     for i in range(NUM_USERS):
-            username = "test" + str(i)
-            password = "test" + str(i) + "password"
-            first_name = str(i) + "First"
-            last_name = str(i) + "Last" 
+            username = "test" + str(i+1)
+            password = "test" + str(i+1) + "password"
+            first_name = str(i+1) + "First"
+            last_name = str(i+1) + "Last" 
             user = [username, password, first_name, last_name]
             user_list.append(user)
 
